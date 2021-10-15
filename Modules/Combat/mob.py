63126cfb56902d2b9bef81d3f9913dff0effe223
Flaguia <setze.ewen@gmail.com>
2021-10-15 21:03:01 +0200

    kdo

---
+++ Modules/Combat/mob.py
@@ -0,0 +1,16 @@
+class Mob:
+
+	def __init__(self, nom, éléments, vie, attaque, magie, mana, vitesse, ,ability = []):
+		self.nom = nom 
+		
+		self.ability = ability
+		self.vie = vie
+		self.attaque = attaque
+		self.magie = magie
+		self.mana = mana
+		self.vitesse = vitesse
+
+	def __str(self):
+		return f'{nom}'
+
+	
