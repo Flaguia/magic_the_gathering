7b23e98e8e388ace97a6f03c435ee5e29e3cf69e
Flaguia <56897567+Flaguia@users.noreply.github.com>
2021-10-19 10:20:47 +0200

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-from Modules import places, Job, Tool, Interface, classe_Joueur, armes, mob, spell
-from random import choice 
-import os
-dic = {}
-dic[1] = Interface.print_formatted_text(Interface.HTML(f'<b><green>ta mere la pute</green></b>'))
-dic[2] = '<b><green>ta mere la pute</green></b>'
-Interface.print_formatted_text(Interface.HTML(dic[2]))
